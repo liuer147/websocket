09449357ae5f6858afaa26b2d175955a7cac9a39
_六耳_ <yuanliu147@gmail.com>
2021-12-23 22:24:34 +0800

    初步完善前后端消息通信

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ul>
+      <li v-for="(item, index) of msgList" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+    <input v-model="msg" />
+    <button @click="handleSubmit">发送</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { reactive, ref } from "vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  setup() {
+    const ws = new WebSocket("ws://127.0.0.1:3000");
+    const msgList = reactive([]);
+    const msg = ref("");
+    ws.onopen = () => {
+      ws.send("我是客户端");
+    };
+    ws.onclose = () => {
+      window.alert(`连接已断开, 状态为: ${ws.readyState}`);
+    };
+    ws.onmessage = (res) => {
+      msgList.push(res.data)
+    };
+    function handleSubmit() {
+      ws.send(`已发送：${msg.value}`);
+      debugger
+      msg.value = "";
+    }
+    return {
+      msgList,
+      msg,
+      handleSubmit,
+    };
   },
 };
 </script>
---
+++ src/views/hooks.js
@@ -0,0 +1,4 @@
+export function useInitEvents(ws) {
+
+
+}
